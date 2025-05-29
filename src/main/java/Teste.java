26437b75d5fceb502cd51a62e393c7ee15a8a87d
Fabio Sato <hidekif620@gmail.com>
2025-05-29 16:54:18 -0300

    ConexaoBD

--- src/main/java/Teste.java
+++ src/main/java/Teste.java
@@ -21,6 +21,13 @@ public class Teste {
     static String driver = "org.postgresql.Driver";
     
     public static void main(String[] args) {
+        Statement st = null;
+        String sql1 = "CREATE TABLE cliente("
+                        + "id int primary key not null,"
+                        + "nome text," 
+                        + "email text)";
+          
+        
         
         java.awt.EventQueue.invokeLater(new Runnable(){
             public void run(){
@@ -30,7 +37,7 @@ public class Teste {
         );
 
         try{ //bloco para registro do driver
-            System.out.println("Carregando o driver..");
+            System.out.println("Carregando o driver...");
             Class.forName(driver);
             System.out.println("Driver carregado com sucesso!");
         }catch(Exception e){
@@ -40,9 +47,40 @@ public class Teste {
             System.out.println("Conectando ao BD...");
             conn = DriverManager.getConnection(url,user,senha);
             System.out.println("Banco conectado com sucesso!");
+            
+            st = conn.createStatement();
+            st.executeUpdate(sql1);
+            System.out.println("Tabela criada com sucesso!");
+            st.close();
+            conn.close();
+            
         }catch(Exception e){
             System.out.println("Falha na conexao!");
         }
+        
+        String sql2 = "INSERT INTO cliente VALUES(1,'Gisele', 'gi@utfpr.edu.br')";
+        
+        try{ //bloco para registro do driver
+            System.out.println("Carregando o driver...");
+            Class.forName(driver);
+            System.out.println("Driver carregado com sucesso!");
+        }catch(Exception e){
+            System.out.println("Falha na conexao!");
+        }
+        try{ //bloco para conexao com BD
+            
+            conn = DriverManager.getConnection(url,user,senha);
+            System.out.println("Inserindo dados....");
+            st = conn.createStatement();
+            st.executeUpdate(sql2);
+            System.out.println("Dados inseridos com sucesso!");
+            st.close();
+            conn.close();
+            
+        }catch(Exception e){
+            System.out.println("Falha na conexao!");
+        }
+        
     }   
 }
 
